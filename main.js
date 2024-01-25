4635bd12c5efc50202f07e565d89e41b68a22e25
Aleksandar Bursac <bursaca@sheridancollege.ca>
2024-01-25 17:03:48 -0500

    fixed page transition

--- main.js
+++ main.js
@@ -29,49 +29,51 @@ function getColor(distance) {
 
 function handleNavbarOptionClick(navbarOption) {
     if (navbarOption.id !== 'github') {
-        var body = document.querySelector('body');
-        var main = document.querySelector('main');
-        var navbar = document.querySelector('nav');
-        body.style.transition = 'all 0.5s ease';
-        main.style.transition = 'all 0.5s ease';
-        navbar.style.transition = 'all 0.5s ease';
-        main.style.transform = 'translateX(-105%)';
-        navbar.style.opacity = '0';
-        setTimeout(function () {
-            body.style.opacity = '0';
-        }, 200);
-        setTimeout(function () {
-            if (navbarOption.id === 'projects') {
-                window.location.href = 'projects.html';
-            } else if (navbarOption.id === 'info') {
-                window.location.href = 'info.html';
-            } else if (navbarOption.id === 'contact') {
-                window.location.href = 'contact.html';
-            }
-        }, 800);
+        if (navbarOption.id === 'projects') {
+            window.location.href = 'projects.html';
+        } else if (navbarOption.id === 'info') {
+            window.location.href = 'info.html';
+        } else if (navbarOption.id === 'contact') {
+            window.location.href = 'contact.html';
+        }
     }
 }
 
-window.onbeforeunload = function() {
-    var body = document.querySelector('body');
-    var main = document.querySelector('main');
-    var navbar = document.querySelector('nav');
-    body.style.transition = '';
-    main.style.transition = '';
-    navbar.style.transition = '';
-    main.style.transform = '';
-    navbar.style.opacity = '';
-    body.style.opacity = '';
-};
+// archived animation code
 
-window.onpopstate = function(event) {
-    var body = document.querySelector('body');
-    var main = document.querySelector('main');
-    var navbar = document.querySelector('nav');
-    body.style.transition = 'all 0.5s ease';
-    main.style.transition = 'all 0.5s ease';
-    navbar.style.transition = 'all 0.5s ease';
-    main.style.transform = '';
-    navbar.style.opacity = '';
-    body.style.opacity = '';
-};
+// function handleNavbarOptionClick(navbarOption) {
+//     if (navbarOption.id !== 'github') {
+//         var body = document.querySelector('body');
+//         var main = document.querySelector('main');
+//         var navbar = document.querySelector('nav');
+//         body.style.transition = 'all 0.5s ease';
+//         main.style.transition = 'all 0.5s ease';
+//         navbar.style.transition = 'all 0.5s ease';
+//         main.style.transform = 'translateX(-105%)';
+//         navbar.style.opacity = '0';
+//         setTimeout(function () {
+//             body.style.opacity = '0';
+//         }, 200);
+//         setTimeout(function () {
+//             if (navbarOption.id === 'projects') {
+//                 window.location.href = 'projects.html';
+//             } else if (navbarOption.id === 'info') {
+//                 window.location.href = 'info.html';
+//             } else if (navbarOption.id === 'contact') {
+//                 window.location.href = 'contact.html';
+//             }
+//         }, 800);
+//     }
+// }
+
+// window.onbeforeunload = function() {
+//     var body = document.querySelector('body');
+//     var main = document.querySelector('main');
+//     var navbar = document.querySelector('nav');
+//     body.style.transition = '';
+//     main.style.transition = '';
+//     navbar.style.transition = '';
+//     main.style.transform = '';
+//     navbar.style.opacity = '';
+//     body.style.opacity = '';
+// };
